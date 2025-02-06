329dea796eb37069ee80f6f4ab0d4ba25562187e
revers-esrever <cry3far3@mail.ru>
2025-02-06 20:56:31 +0500

    работа с классом Router part 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,4 +13,27 @@ Route::any('/dashboard', function () {
 
 Route::match(['get,post'],'/users', function () {
     return ['john', 'alex', 'kara'];
+});
+
+//Использование параметров в маршрутах
+Route::get('/transactions/{transactionId}', function($transactionId){
+    return "transaction #$transactionId was processed";
+});
+Route::get('/transactions/{transactionId}/files/{fileId}', function(int $transactionId,int $filesId){
+    return "transaction #$transactionId was processed in file $filesId";
+});
+
+//Необязательные параметры
+//Добавление регeлярного выражения через where
+Route::get('/report/{year}/{month?}', function($year, $month = null){
+    return "report date: $year $month";
+})->where('year', '[0-9]+');
+
+//Использование параметров строки запроса
+
+Route::get('/report', function (Request $request) {
+    $year = $request->get('year');
+    $month = $request->get('month');
+
+    return "report. Query string params: year: $year, month:$month";
 });
